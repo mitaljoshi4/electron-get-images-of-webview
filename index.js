d6f452c829e322c9cb9d869d834b140f7356c3e1
Mital Joshi <tbs.mital@gmail.com>
2017-10-23 20:07:47 +0530

    added csv parsing

--- index.js
+++ index.js
@@ -1,33 +1,66 @@
 var request = require('request');
-var fs = require('fs');
-
+var Papa = require('./papaParse.js');
 var imagURL;
+var selector = "";
 var webview = document.getElementById("webview");
+var fileInput = document.getElementById("chooseFileInput");
 webview.addEventListener("dom-ready", function () {
-
-    var downloadBtn = document.getElementById('downloadBtn');
-    var inputText = document.getElementById('selectorInput');
-
-    downloadBtn.addEventListener('click', function (e, data) {
-        var selector = inputText.value;
-        console.log(selector);
-        webview.send("find-element", {
-            text: selector
-        });
+    webview.send("find-element", {
+        text: selector
     });
 });
+
 
 webview.addEventListener('ipc-message', function (event) {
     console.log(event);
     console.info(event.channel);
-
     var downloadBtn = document.getElementById('downloadBtn');
-
-    var imageArray = JSON.parse(event.channel);
-    if (imageArray.length > 0) {
-        // downloadBtn.href = imageArray[0];
-        // downloadBtn.download = "image.png";
-       webview.send('dowloadFiles', imageArray[0]);
-        // downloadBtn(imageArray[0]);
+    // var imageArray = JSON.parse(event.channel);
+    // if (imageArray.length > 0) {
+    downloadBtn.href = event.channel;
+    downloadBtn.download = "image.png";
+    var image = document.createElement("img");
+    image.src = event.channel;
+    document.getElementsByTagName("body")[0].appendChild(image);
+    // }
+});
+fileInput.addEventListener('change', function (evt) {
+    var file = evt.target.files[0];
+    var downloadfileBtn = document.getElementById("downloadCSVBtn");
+    downloadfileBtn.href = file.path;
+    downloadfileBtn.download = "DemoFile.csv";
+    if (file != undefined) {
+        var ext = file.name.slice((file.name.lastIndexOf(".") - 1 >>> 0) + 2);
+        console.log(ext);
+        if (ext == 'csv') {
+            Papa.parse(file, {
+                header: true,
+                skipEmptyLines: true,
+                dynamicTyping: true,
+                complete: function (results) {
+                    data = results;
+                    var outputObj = {};
+                    outputObj.products = data.data;
+                    console.log(data.meta.fields);
+                    var arr = data.meta.fields;
+                    if (arr.length == 2 && arr.indexOf("link") >= 0 && arr.indexOf("selector") >= 0) {
+                        console.log(JSON.stringify(outputObj, null, 2));
+                        var i = 0;
+                        var objLength = outputObj.products.length;
+                        for (i = 0; i < objLength; i++) {
+                            webview.loadURL(outputObj.products[i].link);
+                            selector = outputObj.products[i].selector;
+                            var isLoaded = webview.isLoading();
+                        }
+                    }
+                    else {
+                        console.log("No data found.");
+                    }
+                }
+            });
+        }
+        else {
+            alert("Please choose CSV file.");
+        }
     }
 });
